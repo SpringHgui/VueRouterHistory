f3df6584e4b30edf7a19bd6229f21294617f5578
Gui.H <springhgui@outlook.com>
2022-05-08 23:32:47 +0800

    init

---
+++ VueRouterHistory/VueDefaultFilesExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VueRouterHistory;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class VueDefaultFilesExtensions
+    {
+        /// <summary>
+        /// Enables default file mapping on the current path
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseVueRouterHistory(this IApplicationBuilder app)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            return app.UseMiddleware<VueDefaultFilesMiddleware>();
+        }
+
+        /// <summary>
+        /// Enables default file mapping on the current path
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseVueRouterHistory(this IApplicationBuilder app, DefaultFilesOptions options)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            return app.UseMiddleware<VueDefaultFilesMiddleware>(Options.Create(options));
+        }
+    }
+}
---
+++ VueRouterHistory/VueDefaultFilesMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace VueRouterHistory
+{
+    public class VueDefaultFilesMiddleware
+    {
+        private readonly DefaultFilesOptions _options;
+        private readonly PathString _matchUrl;
+        private readonly RequestDelegate _next;
+        private readonly IWebHostEnvironment _hostingEnv;
+
+        public VueDefaultFilesMiddleware(RequestDelegate next, IWebHostEnvironment hostingEnv, IOptions<DefaultFilesOptions> options)
+        {
+            if (next == null)
+            {
+                throw new ArgumentNullException(nameof(next));
+            }
+
+            if (hostingEnv == null)
+            {
+                throw new ArgumentNullException(nameof(hostingEnv));
+            }
+
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            _next = next;
+            _options = options.Value;
+            _hostingEnv = hostingEnv;
+            _matchUrl = _options.RequestPath;
+        }
+
+        internal static bool IsGetOrHeadMethod(string method)
+        {
+            return HttpMethods.IsGet(method) || HttpMethods.IsHead(method);
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.GetEndpoint() == null && IsGetOrHeadMethod(context.Request.Method))
+            {
+                var path = Path.Join(_hostingEnv.WebRootPath, context.Request.Path);
+                var name = Path.Combine(Path.GetDirectoryName(path)!, "index.html");
+                if (File.Exists(name))
+                {
+                    context.Response.StatusCode = 200;
+                    await context.Response.SendFileAsync(name);
+                }
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+}
